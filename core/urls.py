39e57b35726c53aaae2c2a93d6fa11954ea143dc
Humoyun Akbaraliev <136078810+humoyunswe@users.noreply.github.com>
2025-08-31 09:35:14 +0500

    add settings.py again

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@
 URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,14 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from core import settings
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- vercel_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Vercel build script
-pip install -r api/requirements.txt
-python manage.py collectstatic --noinput
